88c62749a0d5423622332673b83be9886f72ea98
demo-git <snpsuaidshema@gmail.com>
2026-04-24 17:07:03 +0530

    added backend code

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["pet_clinic"]
-appointments = db["appointments"]
+appointments = []   # temporary storage
 
 @app.route("/api/appointments", methods=["POST"])
 def book_appointment():
     data = request.json
 
-    appointments.insert_one(data)
+    appointments.append(data)
 
     return jsonify({"message": "Appointment booked successfully!"})
+
+@app.route("/api/appointments", methods=["GET"])
+def get_appointments():
+    return jsonify(appointments)
 
 if __name__ == "__main__":
     app.run(debug=True)
